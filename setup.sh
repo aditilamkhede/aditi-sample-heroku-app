de6fd72e5e283bcd0181eb797ccf1d9b00241ccf
Aditi Lamkhede <aditi.lamkhede@gmail.com>
2020-05-01 18:55:27 -0700

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL="postgres://dlmxmuygizaszt:d491690210c2472dd2a7a6da3405fa3ba812a5d487b38391890805519c1146fe@ec2-34-195-169-25.compute-1.amazonaws.com:5432/dcjmrho3d1uknn"
+# "postgres://localhost:5432/capstone"
+export EXCITED=True
